9b7521545bf33c2373525c04f9b1c56ac2d09fd7
Shlok Warukar <warulkarshlok@gmail.com>
2026-06-25 22:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (30.17%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,23 +1,47 @@
-class Solution {
-    public char nextGreatestLetter(char[] letters, char target) {
-        HashSet<Character> set = new HashSet<>();
-        int num = 0;
-        char ch = 'a';
-        char ans = letters[0];
-        for(char c : letters){
-            set.add(c);
-        }
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         HashSet<Character> set = new HashSet<>();
+//         int num = 0;
+//         char ch = 'a';
+//         char ans = letters[0];
+//         for(char c : letters){
+//             set.add(c);
+//         }
 
-        for(int i =1;i<26;i++){
-            num = target + i;
-            ch = (char) num;
-             if(set.contains(ch)){
-            ans = ch;
-            break;
-        }
-        }
+//         for(int i =1;i<26;i++){
+//             num = target + i;
+//             ch = (char) num;
+//              if(set.contains(ch)){
+//             ans = ch;
+//             break;
+//         }
+//         }
        
 
-        return ans ;
+//         return ans ;
+//     }
+// }
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        int left = 0;
+        int right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+       if (left == letters.length) {
+    return letters[0];
+} else {
+    return letters[left];
+}
     }
 }
