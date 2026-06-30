6b2fe59b1834d0b51c1f61f644eed3d513da8e0f
Shlok Warukar <warulkarshlok@gmail.com>
2026-06-30 15:30:35 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (24.97%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,21 +1,40 @@
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+
+//         for(int key : nums){
+//             map.put(key,map.getOrDefault(key,0)+1);
+
+//         }
+//         for(Map.Entry<Integer, Integer> e : map.entrySet()){
+//             if(e.getValue()%2!=0){
+//                 return false;
+//             }
+//         }
+//         // for (int freq : map.values()) {
+//         // if (freq % 2 != 0) {
+//         // return false;
+//         // }
+//         // }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean divideArray(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int key : nums){
-            map.put(key,map.getOrDefault(key,0)+1);
+        int[] freq = new int[501];
 
+        for (int num : nums) {
+            freq[num]++;
         }
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-            if(e.getValue()%2!=0){
+
+        for (int count : freq) {
+            if (count % 2 != 0) {
                 return false;
             }
         }
-        // for (int freq : map.values()) {
-        // if (freq % 2 != 0) {
-        // return false;
-        // }
-        // }
+
         return true;
     }
 }
