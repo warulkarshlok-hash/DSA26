b51a057ddf4192fa76da92bea97c50c3117b8a98
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-02 23:04:22 +0530

    Time: 2 ms (99.79%) | Memory: 69.5 MB (62.23%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,27 +1,46 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-     int i = 0;
-     int p = 0;
-     double sum = 0;
-     double max = Integer.MIN_VALUE;
-     double avg = 0;
-     for(int j = 0;j<k;j++){
-        sum+=nums[j];
-        p=j+1;
-     }
-        avg = sum/k;
-        max=Math.max(max,avg);
-
-        while(p<nums.length){
-            sum+=nums[p];
-            sum-=nums[i];
-            avg = sum/k;
-            max=Math.max(max,avg);
-
-            p++;
-            i++;
-            
+        //OPTIMIZED 
+        int sum = 0;
+        for(int i = 0;i<k;i++){
+            sum+=nums[i];
         }
-        return max;
+        int maxSum = sum;
+        for(int i = k;i<nums.length;i++){
+            sum = sum - nums[i-k] + nums[i];
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+        }
+        return (double) maxSum/k;
+
+
+        //BRUTE FORCE
+    //  int i = 0;
+    //  int p = 0;
+    //  //double is important thing here 
+    //  double sum = 0;
+    //  double max = Integer.MIN_VALUE;
+    //  double avg = 0;
+    //  for(int j = 0;j<k;j++){
+    //     sum+=nums[j];
+    //     p=j+1;
+    //  }
+        // avg = sum/k;
+        // max=Math.max(max,avg);
+
+        // while(p<nums.length){
+        //     sum+=nums[p];
+        //     sum-=nums[i];
+        //     avg = sum/k; // To get accurate result atleast one should be double 
+        //     max=Math.max(max,avg);
+
+        //     p++;
+        //     i++;
+            
+        // }
+        // return max;
+    
     }
+
 }
