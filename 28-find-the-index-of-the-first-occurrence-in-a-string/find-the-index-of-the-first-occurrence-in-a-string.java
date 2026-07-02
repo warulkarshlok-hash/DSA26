9bb24e76d41875d38849c3bce190e104b482c33d
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-02 20:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (18.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,15 +1,22 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        //BUILT IN 1
         // if(haystack.contains(needle)){
-            
         //     return haystack.indexOf(needle);
-
         // }
         // else{
-            
         //     return -1;
         // }
 
-        return haystack.indexOf(needle);
+        //BUILT IN 2
+        // return haystack.indexOf(needle);
+
+        // OPTIMIZED USING SUBSTRING METHOD
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
