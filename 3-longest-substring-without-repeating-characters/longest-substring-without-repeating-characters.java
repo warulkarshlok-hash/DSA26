b7334c45a1af269090900c18c24e00d515581215
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-02 23:53:52 +0530

    Time: 7 ms (36.90%) | Memory: 46.4 MB (47.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,22 +1,44 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        //OPTIMIZED USING ONE WHILE LOOP: 
         HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int right =0;
         int max = Integer.MIN_VALUE;
-        int i = 0;
-        while(i<s.length()){
-            for(int j = i;j<s.length();j++){
-                if(!set.contains(s.charAt(j))){
-                    set.add(s.charAt(j));
-                    max = Math.max(max,set.size());
-                }
-                else{
-                    i++;
-                    set.clear();
-                    break; // NAHITR J UPDATE(J=I) NA HOTA ,NEXT VALUE GHEIL ,I.E J++; 
-                }
+        while(right<s.length()){
+            
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                right++;
+                max=Math.max(max,set.size());
             }
+            else{
+                set.remove(s.charAt(left)); //fakt magcha element remove kraycha , insted of starting right from left again , makes it run it O(n)
+                left++;
+            }
+
         }
-        return s.length()==0? 0:max;
+        return s.length()==0? 0:max; //EDGE CASE : 0 sathi 0 answer 
+
     }
 }
+            //BRUTE FORCE : 87MS
+        // HashSet<Character> set = new HashSet<>();
+        // int max = Integer.MIN_VALUE;
+        // int i = 0;
+        // while(i<s.length()){
+        //     for(int j = i;j<s.length();j++){
+        //         if(!set.contains(s.charAt(j))){
+        //             set.add(s.charAt(j));
+        //             max = Math.max(max,set.size());
+        //         }
+        //         else{
+        //             i++;
+        //             set.clear();
+        //             break; // NAHITR J UPDATE(J=I) NA HOTA ,NEXT VALUE GHEIL ,I.E J++; 
+        //         }
+        //     }
+        // }
+        // return s.length()==0? 0:max;
+
+
