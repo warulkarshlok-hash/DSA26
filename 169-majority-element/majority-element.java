3b75df740e5f64f4281bc9b3e005d4a8ad165af2
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-03 21:52:00 +0530

    Time: 1 ms (99.89%) | Memory: 55.5 MB (59.40%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       HashMap<Integer,Integer> map = new HashMap<>();
-       int ans = 0 ;
+        
+        //Moore Voting(NullyFying) Algorithm: 
+        int majority = nums[0];
+        int count = 1;
 
-       for(int num : nums){
-        map.put(num,map.getOrDefault(num,0)+1);
-       }
-
-       int limit = nums.length/2;
-
-       for(int key : map.keySet()){
-        if(map.get(key)>limit){
-            ans = key;
+        for(int i =  1;i<nums.length;i++){
+            if(count==0){
+                majority = nums[i];
+                count = 1;
+            }
+            else if(majority==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-       }
-       return ans ;
+        return majority;
+
+
+        //BRUTE FORCE USING HASHMAP : 13ms
+    //    HashMap<Integer,Integer> map = new HashMap<>();
+    //    int ans = 0 ;
+
+    //    for(int num : nums){
+    //     map.put(num,map.getOrDefault(num,0)+1);
+    //    }
+
+    //    int limit = nums.length/2;
+
+    //    for(int key : map.keySet()){
+    //     if(map.get(key)>limit){
+    //         ans = key;
+    //     }
+    //    }
+    //    return ans ;
   }
 }
