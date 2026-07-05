b4df9ff1293f37a4f37304c4ef07ed68392e19de
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-05 19:24:41 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (100.00%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.java
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -1,18 +1,29 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
-        int n = nums.length;
-        int middle = n/2;
-        int midElement = nums[middle];
-        int[] freq = new int[101];
+        // int n = nums.length;
+        // int middle = n/2;
+        // int midElement = nums[middle];
+        // int[] freq = new int[101];
 
-        for(int num : nums){
-            freq[num]++;
-        } 
+        // for(int num : nums){
+        //     freq[num]++;
+        // } 
 
-        if(freq[midElement]!=1){
-            return false;
-        }
+        // if(freq[midElement]!=1){
+        //     return false;
+        // }
         
-        return true;
+        // return true;
+
+        // in just one for loop 
+        int midElement = nums[nums.length/2];
+        int count= 0;
+        for(int num : nums){
+            if(num == midElement){
+                count++;
+            }
+        }
+
+        return count==1? true : false;
     }
 }
