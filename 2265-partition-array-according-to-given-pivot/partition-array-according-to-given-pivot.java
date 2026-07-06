c429f93648757e69e773df7044b82a35752c67cf
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-06 22:17:00 +0530

    Time: 5 ms (72.58%) | Memory: 160 MB (42.58%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,28 +1,55 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
-        int[] arr = new int[n];
-        
-        int left = 0;
-        int right = n-1;
-        int start = 0;
-        int end = n-1;
-        while(left < n && right>=0){
-            if(nums[left]<pivot){
-                arr[start]=nums[left];
-                start++;
+        // TWO POINTER APPROACH : 5ms ,150MB
+        // int n = nums.length;
+        // int[] arr = new int[n];
+        // int left = 0;
+        // int right = n-1;
+        // int start = 0;
+        // int end = n-1;
+        // while(left < n && right>=0){
+        //     if(nums[left]<pivot){
+        //         arr[start]=nums[left];
+        //         start++;
+        //     }
+        //     left++;
+        //     if(nums[right]>pivot){
+        //         arr[end]=nums[right];
+        //         end--;
+        //     }
+        //     right--;
+        // }
+        // while (start <= end) {
+        //     arr[start] = pivot;
+        //     start++;
+        // }
+        // return arr;
+
+        //3 FOR LOOP APPROACH : 
+        int[] ans = new int[nums.length];
+        int index = 0;
+
+        // Elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[index++] = num;
             }
-            left++;
-            if(nums[right]>pivot){
-                arr[end]=nums[right];
-                end--;
+        }
+
+        // Elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[index++] = num;
             }
-            right--;
         }
-        while (start <= end) {
-            arr[start] = pivot;
-            start++;
+
+        // Elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[index++] = num;
+            }
         }
-        return arr;
+
+        return ans;
     }
 }
