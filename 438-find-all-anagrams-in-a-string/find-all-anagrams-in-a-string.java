84034bad97b2b7ff3f181ae5770907b3b2ffa9a0
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-09 17:59:11 +0530

    Time: 9 ms (92.55%) | Memory: 46.8 MB (72.42%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,36 +1,30 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        int n1 = s.length();  
+        int n2 = p.length();  
 
         List<Integer> ans = new ArrayList<>();
 
-        if (p.length() > s.length()) return ans;
+        if (n1 < n2) return ans;
 
-        int[] pFreq = new int[26];
-        int[] windowFreq = new int[26];
+        int[] a = new int[26]; 
+        int[] b = new int[26]; 
 
-        // Store frequency of pattern and first window
-        for (int i = 0; i < p.length(); i++) {
-            pFreq[p.charAt(i) - 'a']++;
-            windowFreq[s.charAt(i) - 'a']++;
+        for (int i = 0; i < n2; i++) {
+            a[p.charAt(i) - 'a']++;
+            b[s.charAt(i) - 'a']++;
         }
 
-        // Check first window
-        if (Arrays.equals(pFreq, windowFreq)) {
+        if (Arrays.equals(a, b)) {
             ans.add(0);
         }
 
-        // Slide the window
-        for (int i = p.length(); i < s.length(); i++) {
+        for (int i = n2; i < n1; i++) {
+            b[s.charAt(i) - 'a']++;           
+            b[s.charAt(i - n2) - 'a']--;     
 
-            // Remove left character
-            windowFreq[s.charAt(i - p.length()) - 'a']--;
-
-            // Add new right character
-            windowFreq[s.charAt(i) - 'a']++;
-
-            // Compare frequencies
-            if (Arrays.equals(pFreq, windowFreq)) {
-                ans.add(i - p.length() + 1);
+            if (Arrays.equals(a, b)) {
+                ans.add(i - n2 + 1);
             }
         }
 
