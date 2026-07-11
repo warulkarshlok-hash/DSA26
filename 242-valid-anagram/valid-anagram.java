63a2565502f6e8f0b5dd88e51dadf309da3fecd5
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-11 16:28:21 +0530

    Time: 4 ms (82.27%) | Memory: 44.3 MB (82.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        //brute force : 3ms , 93%
+        // int[] freq = new int[26];
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+
+        // for(char c : s.toCharArray()){
+        //     freq[c-'a']++;
+        // }
+        // for(char ch : t.toCharArray()){
+        //     if(freq[ch-'a'] > 0){
+        //         freq[ch-'a']--;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //Optimized approach : 0ms,100%
         int[] freq = new int[26];
-        if(s.length() != t.length()){
-            return false;
+        int n = s.length(),m=t.length();
+        if(n!=m) return false;
+        for (int i = 0; i < n; i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for(char c : s.toCharArray()){
-            freq[c-'a']++;
-        }
-        for(char ch : t.toCharArray()){
-            if(freq[ch-'a']>0){
-                freq[ch-'a']--;
-            }
-            else{
+        for (int i = 0; i < 26; i++)
+            if (freq[i] != 0)
                 return false;
-            }
-        }
         return true;
     }
 }
