cfd99e85dd3c794aa183cbcbd234966e5126ccb7
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-13 18:14:46 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (52.05%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,20 +1,42 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int[] freq = new int[1001];
 
         for(int num : target){
-            map.put(num,map.getOrDefault(num,0)+1);
+            freq[num]++;
         }
+
         for(int key : arr){
-            if(map.containsKey(key) && map.get(key)>0){
-                map.put(key, map.get(key) - 1);
-                
-            }
-            else{
+            freq[key]--;
+        }
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0){
                 return false;
             }
-           
         }
+
         return true;
+
+
+
+
+        // HashMap<Integer,Integer> map = new HashMap<>();
+
+        // for(int num : target){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
+        // for(int key : arr){
+        //     if(map.containsKey(key) && map.get(key)>0){
+        //         map.put(key, map.get(key) - 1);
+                
+        //     }
+        //     else{
+        //         return false;
+        //     }
+           
+        // }
+        // return true;
     }
 }
