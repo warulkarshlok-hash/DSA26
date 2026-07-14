c32edd71536be7baf2833378dcaaac1f87e05cef
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-14 19:26:39 +0530

    Time: 19 ms (21.26%) | Memory: 45.2 MB (27.58%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -2,31 +2,63 @@ class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
 
         paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
-        //removes everything(except small letters) like ,!.;AND space
-        
         String[] words = paragraph.split("\\s+");
-        // \\s+ : Split the string wherever there are one or more whitespace characters.
 
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> bannedSet = new HashSet<>();
 
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
+        for (String word : banned) {
+            bannedSet.add(word);
         }
 
-        for (String ban : banned) {
-            map.remove(ban);
-        }
+        HashMap<String, Integer> freq = new HashMap<>();
 
-        int max = 0;
+        int maxFreq = 0;
         String ans = "";
 
         for (String word : words) {
-            if (map.containsKey(word) && map.get(word) > max) {
-                max = map.get(word);
+
+            if (bannedSet.contains(word))
+                continue;
+
+            int count = freq.getOrDefault(word, 0) + 1;
+            freq.put(word, count);
+
+            if (count > maxFreq) {
+                maxFreq = count;
                 ans = word;
             }
         }
 
         return ans;
+
+
+        //19ms ,21%
+        // paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
+        // //removes everything(except small letters) like ,!.; AND space
+
+        // String[] words = paragraph.split("\\s+");
+        // // \\s+ : Split the string wherever there are one or more whitespace characters.
+
+        // HashMap<String, Integer> map = new HashMap<>();
+
+        // for (String word : words) {
+        //     map.put(word, map.getOrDefault(word, 0) + 1);
+        // }
+
+        // for (String ban : banned) {
+        //     map.remove(ban);
+        // }
+
+        // int max = 0;
+        // String ans = "";
+
+        // for (String word : words) {
+        //     if (map.containsKey(word) && map.get(word) > max) {
+        //         max = map.get(word);
+        //         ans = word;
+        //     }
+        // }
+
+        // return ans;
     }
 }
