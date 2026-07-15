bcf2af54845205dccb0da7e36e95e4664159564e
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-15 18:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (74.78%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,13 +1,15 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        long sumOdd = n*n;
-        long sumEven = n*(n+1);
+        // long sumOdd = n*n; //formula for sum of n odd nubers 
+        // long sumEven = n*(n+1); //formula for even of n odd nubers 
 
-        long max = Math.max(sumOdd,sumEven);
+        // long max = Math.max(sumOdd,sumEven);
 
-        long min = Math.min(sumOdd,sumEven);
+        // long min = Math.min(sumOdd,sumEven);
 
-        return (int)max-(int)min;
+        // return (int)max-(int)min; //greatest common devisor is just a difference between numbers 
+
+        return n;
 
 
     }
