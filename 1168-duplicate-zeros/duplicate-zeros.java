7b55e070199bddc33c3c21d1d0387859c1ed3a86
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-15 13:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (8.43%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,30 +1,52 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-      int[] nums = new int[arr.length];
 
-      int  i = 0;
-      int j = 0;
-      while(i<arr.length && j<nums.length){
-        if(arr[i] != 0){
-            nums[j] = arr[i];
-           
-            j++;
-        }
-        else{
-            nums[j]=0;
-            if(j+1<nums.length){
-            nums[j+1]=0;
-            
+   
+        int[] k=arr.clone();
+        int i=0,l=0;
+        while(l<arr.length)
+        {
+            if(k[i]==0)
+            {
+                arr[l++]=0;
+                if(l<arr.length)
+                {
+                    arr[l++]=0;
+                }
             }
-            j=j+2;
+                else
+                {
+                    arr[l++]=k[i];
+                }
+                i++;
+            
         }
-        i++;
+    
+
+    //   int[] nums = new int[arr.length];
+
+    //   int  i = 0;
+    //   int j = 0;
+    //   while(i<arr.length && j<nums.length){
+    //     if(arr[i] != 0){
+    //         nums[j] = arr[i];
+           
+    //         j++;
+    //     }
+    //     else{
+    //         nums[j]=0;
+    //         if(j+1<nums.length){
+    //         nums[j+1]=0;
+            
+    //         }
+    //         j=j+2;
+    //     }
+    //     i++;
         
        
-      }
+    //   }
 
-      for(int p = 0;p<arr.length;p++){
-        arr[p]=nums[p];
+    //   for(int p = 0;p<arr.length;p++){
+    //     arr[p]=nums[p];
       }
     }
-}
