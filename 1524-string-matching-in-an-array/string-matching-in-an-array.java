8f558dbc662d4160b36b0566138ca5e349569207
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-17 18:40:53 +0530

    Time: 2 ms (99.78%) | Memory: 43.8 MB (32.31%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,14 +1,30 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (i != j && words[j].contains(words[i])) {
-                list.add(words[i]);
-                break;      // avoid duplicates
+
+    
+        String str = String.join(" ", words);
+        List<String> res = new ArrayList<>();
+        for(int i = 0; i < words.length; i++){
+            if(str.indexOf(words[i]) != str.lastIndexOf(words[i])){ 
+        //IF INDEX ARE DIFFERENT OF FIRST OCCURENCE AND SECOND OCCRENCE MEANS THEY ARE SUBSTRING
+                res.add(words[i]);
+            }
         }
-    }
-}
-        return list;
+        return res;
+    
+        
+
+
+        //4ms , 95%
+    //     ArrayList<String> list = new ArrayList<>();
+    //     for (int i = 0; i < words.length; i++) {
+    //         for (int j = 0; j < words.length; j++) {
+    //                 if (i != j && words[j].contains(words[i])) {
+    //                 list.add(words[i]);
+    //                 break;      // avoid duplicates
+    //             }
+    //         }
+    //     }
+    // return list;
     }
 }
