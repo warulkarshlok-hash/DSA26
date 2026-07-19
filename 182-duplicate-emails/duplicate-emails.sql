e8318a01bd873306e4ea16d9efb00d756260e5b7
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-19 23:58:31 +0530

    Time: 337 ms (99.05%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
 
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(*)>1;
+-- SELECT email
+-- FROM Person
+-- GROUP BY email
+-- HAVING COUNT(*)>1;
+
+select email from person 
+group by email
+having count(email) > 1
