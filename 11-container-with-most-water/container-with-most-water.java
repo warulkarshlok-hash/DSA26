795b60af9b0bfda7064a6c34a90edadbbe26240c
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-28 00:38:26 +0530

    Time: 5 ms (82.83%) | Memory: 77.5 MB (12.65%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,35 +1,58 @@
 class Solution {
     public int maxArea(int[] height) {
-        int MaxArea = 0;
-        int left = 0;
-        int right = height.length-1;
-        int minHeight = 0;
-        int length = 0;
-        int currArea = 0;
         
+        int left = 0 ;
+        int right = height.length - 1 ;
+        int max = 0 ;
+
         while(left < right){
-            
-            if(height[left] == height[right]){
-                minHeight = height[left];
-               
-            }
-            else{
-                minHeight = Math.min(height[left],height[right]);
-            }
 
-            length = right-left;
-            currArea = minHeight * length;
-            MaxArea = Math.max(MaxArea,currArea);
+            int width = right - left ;
+            int heigh = Math.min(height[left],height[right]);
 
-            if(height[left]<height[right]){
+            int area = width * heigh ;
+            max = Math.max(max,area);
+
+            if(height[left] < height[right]){
                 left++;
+            } else {
+                right --;
             }
-            else if(height[left]>=height[right]){
-                right--;
-            }
-            
         }
+        return max ;
 
-        return MaxArea;
+
+
+        // int MaxArea = 0;
+        // int left = 0;
+        // int right = height.length-1;
+        // int minHeight = 0;
+        // int length = 0;
+        // int currArea = 0;
+        
+        // while(left < right){
+            
+        //     if(height[left] == height[right]){
+        //         minHeight = height[left];
+               
+        //     }
+        //     else{
+        //         minHeight = Math.min(height[left],height[right]);
+        //     }
+
+        //     length = right-left;
+        //     currArea = minHeight * length;
+        //     MaxArea = Math.max(MaxArea,currArea);
+
+        //     if(height[left]<height[right]){
+        //         left++;
+        //     }
+        //     else if(height[left]>=height[right]){
+        //         right--;
+        //     }
+            
+        // }
+
+        // return MaxArea;
     }
 }
