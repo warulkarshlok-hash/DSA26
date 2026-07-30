c93f45c81a4d76ee595948eda2db34d84af9917f
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-30 18:38:56 +0530

    Time: 1 ms (99.98%) | Memory: 46.2 MB (89.90%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,25 +1,49 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine){
-        HashMap<Character,Integer> map = new HashMap<>();
+
+
+        //using FREQ ARRAY : 
+        if(ransomNote.length() > magazine.length()){
+            return false;
+        }    
+
+        int[] freq = new int[26];
 
         for(char c : magazine.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-
-        }
-
-        for(char key : ransomNote.toCharArray()){
-            if(!map.containsKey(key)){
+            freq[c - 'a']++;
+        }    
+        
+        for(char c : ransomNote.toCharArray()){
+            if(freq[c - 'a'] == 0){
                 return false;
             }
-            else if(map.containsKey(key) && map.get(key) != 0){
-                map.put(key,map.get(key)-1);
-
-            }
-            else{
-                return false;
-            }
+            freq[c - 'a']--;    
         }
-
         return true;
+        
+   
+
+        //Using HASHMAP : 15ms & 30%
+        // HashMap<Character,Integer> map = new HashMap<>();
+
+        // for(char c : magazine.toCharArray()){
+        //     map.put(c,map.getOrDefault(c,0)+1);
+
+        // }
+
+        // for(char key : ransomNote.toCharArray()){
+        //     if(!map.containsKey(key)){
+        //         return false;
+        //     }
+        //     else if(map.containsKey(key) && map.get(key) != 0){
+        //         map.put(key,map.get(key)-1);
+
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+
+        // return true;
     }
 }
