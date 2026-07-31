13a3e3c8cd1a75244e67a87ac55360298abc81e8
Shlok Warukar <warulkarshlok@gmail.com>
2026-07-31 11:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (91.62%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,27 +1,52 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-    int left = 0;
-    int right = s.length()-1;
-    char[] ch = s.toCharArray();
+    // int left = 0;
+    // int right = s.length()-1;
+    // char[] ch = s.toCharArray();
 
-        while(left<right){
-            if(Character.isLetter(ch[left]) && Character.isLetter(ch[right])){
-                char temp = ch[right];
-                ch[right] = ch[left];
-                ch[left]=temp;
-                left++;
-                right--;
-            }
-            else if(!Character.isLetter(ch[left])){
-                left++;
-            }
-            else if(!Character.isLetter(ch[right])){
-                right--;
-            }
+    //     while(left<right){
+    //         if(Character.isLetter(ch[left]) && Character.isLetter(ch[right])){
+    //             char temp = ch[right];
+    //             ch[right] = ch[left];
+    //             ch[left]=temp;
+    //             left++;
+    //             right--;
+    //         }
+    //         else if(!Character.isLetter(ch[left])){
+    //             left++;
+    //         }
+    //         else if(!Character.isLetter(ch[right])){
+    //             right--;
+    //         }
 
+    //     }
+
+         
+    //     return new String(ch);
+
+        char[] arr = s.toCharArray();
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+
+            if (!Character.isLetter(arr[i])) {
+                i++;
+            }
+            else if (!Character.isLetter(arr[j])) {
+                j--;
+            }
+            else {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+                j--;
+            }
         }
 
-        String ans  = new String(ch);
-        return ans;
+        return new String(arr);
     }
 }
