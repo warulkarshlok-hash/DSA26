751e32570a9d9c4b6533b6eea9df3905580dd144
Shlok Warukar <warulkarshlok@gmail.com>
2026-08-08 23:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (86.86%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -1,11 +1,24 @@
 class Solution {
     public int firstMatchingIndex(String s) {
-        int n = s.length();
-       for(int i =  0;i<=n/2;i++){
-        if(s.charAt(i)==s.charAt(n-i-1)){
-            return i;
+
+        int left = 0;
+        int right = s.length()-1;
+        while(left<=right){
+        
+        if(s.charAt(left)==s.charAt(right)){
+            return left;
+        }    
+            left++;
+            right = s.length()-left-1;
         }
-       }
-       return -1;
+        return -1;
+
+    //     int n = s.length();
+    //    for(int i =  0;i<=n/2;i++){
+    //     if(s.charAt(i)==s.charAt(n-i-1)){
+    //         return i;
+    //     }
+    //    }
+    //    return -1;
     }
 }
