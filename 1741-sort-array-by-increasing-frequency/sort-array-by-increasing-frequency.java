1d72e24ed144d9b4711893f0e538477d6c3ba918
Shlok Warukar <warulkarshlok@gmail.com>
2026-08-13 11:46:06 +0530

    Time: 9 ms (44.46%) | Memory: 45.5 MB (66.13%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+
+        for (int n : nums) {                            
+            ans.add(n);
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        Collections.sort(ans, (a, b) ->                 
+            (map.get(a) == map.get(b))? b - a : map.get(a) - map.get(b)
+        );
+        int[] result = new int[ans.size()];
+        for(int i = 0;i<ans.size();i++){
+            result[i]=ans.get(i);
+
+        }
+        return result;
+    }
+}
+
+// TC: O(n) + O(n * logn) + O(n) => O(n * logn)
+// SC: O(n)
