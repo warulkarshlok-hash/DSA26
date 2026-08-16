d2bd259ba9f62d16b92db3293d305041f7409801
Shlok Warukar <warulkarshlok@gmail.com>
2026-08-16 11:36:23 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (67.15%) - LeetSync

--- 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.java
@@ -1,18 +1,35 @@
 class Solution {
     public long removeZeros(long n){
-      String s = "";
-      while(n>0){
-        s=(char)('0'+n%10)+s;
-        n=n/10;
-      }
 
-      String ans ="";
-      for(char ch : s.toCharArray()){
-        if(ch !='0'){
-            ans+=ch;
+    long res = 0;
+    long mul = 1;
+    while(n > 0){
+        int d = (int) (n % 10);
+        if(d != 0) {
+            res = res  + d * mul;
+            mul *= 10;
         }
-      }
-    //   System.out.println(ans);
-      return Long.parseLong(ans);
+        n /= 10;
+    }
+
+    return res;
+ 
+
+
+    //   8ms , 6%  
+    //   String s = "";
+    //   while(n>0){
+    //     s=(char)('0'+n%10)+s;
+    //     n=n/10;
+    //   }
+
+    //   String ans ="";
+    //   for(char ch : s.toCharArray()){
+    //     if(ch !='0'){
+    //         ans+=ch;
+    //     }
+    //   }
+    // //   System.out.println(ans);
+    //   return Long.parseLong(ans);
     }
 }
