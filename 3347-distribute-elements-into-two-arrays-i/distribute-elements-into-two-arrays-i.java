79ae36edd3be6b1f8af1b3006455989fcf1e4335
Shlok Warukar <warulkarshlok@gmail.com>
2026-08-20 15:13:55 +0530

    Time: 1 ms (98.08%) | Memory: 46.6 MB (66.35%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,34 +1,69 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
 
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
+   
+    int n = nums.length;
 
-        for(int i = 2;i<nums.length;i++){
-            if(arr1.get(arr1.size()-1)>arr2.get(arr2.size()-1)){
-                arr1.add(nums[i]);
-            }
-            else{
-                arr2.add(nums[i]);
-            }
+    int[] arr1 = new int[n];
+    int[] arr2 = new int[n];
+
+    int s1 = 1;
+    int s2 = 1;
+
+    arr1[0] = nums[0];
+    arr2[0] = nums[1];
+
+    for (int i = 2; i < n; i++) {
+        if (arr1[s1 - 1] > arr2[s2 - 1]) {
+            arr1[s1++] = nums[i];
+        } else {
+            arr2[s2++] = nums[i];
         }
-        int[] result = new int[nums.length];
+    }
 
-        int k = 0;
+    int[] result = new int[n];
 
-        // Add arr1 elements
-        for (int i = 0; i < arr1.size(); i++) {
-            result[k++] = arr1.get(i);
-        }
+    // Copy arr1
+    for (int i = 0; i < s1; i++) {
+        result[i] = arr1[i];
+    }
 
-        // Add arr2 elements
-        for (int i = 0; i < arr2.size(); i++) {
-            result[k++] = arr2.get(i);
-        }
+    // Copy arr2
+    for (int i = 0; i < s2; i++) {
+        result[s1 + i] = arr2[i];
+    }
 
-        return result;
+    return result;
+ 
+        // ArrayList<Integer> arr1 = new ArrayList<>();
+        // ArrayList<Integer> arr2 = new ArrayList<>();
+
+        // arr1.add(nums[0]);
+        // arr2.add(nums[1]);
+
+        // for(int i = 2;i<nums.length;i++){
+        //     if(arr1.get(arr1.size()-1)>arr2.get(arr2.size()-1)){
+        //         arr1.add(nums[i]);
+        //     }
+        //     else{
+        //         arr2.add(nums[i]);
+        //     }
+        // }
+        // int[] result = new int[nums.length];
+
+        // int k = 0;
+
+        // // Add arr1 elements
+        // for (int i = 0; i < arr1.size(); i++) {
+        //     result[k++] = arr1.get(i);
+        // }
+
+        // // Add arr2 elements
+        // for (int i = 0; i < arr2.size(); i++) {
+        //     result[k++] = arr2.get(i);
+        // }
+
+        // return result;
 
 
 
