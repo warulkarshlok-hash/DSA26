461906c47dc27926cc4ad45a036eff6b8939aca1
Shlok Warukar <warulkarshlok@gmail.com>
2026-09-02 12:31:59 +0530

    Time: 1 ms (31.89%) | Memory: 45.5 MB (5.31%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,29 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        int odd = 0;
+        for(int i = 0;i<nums1.length;i++){
+            if(nums1[i]%2==1){
+                odd=nums1[i];
+                break;
+            }
+        }
+        if(odd==0) return true;
+
+        int[] nums2=new int[nums1.length];
+        
+        for(int j=0;j<nums1.length;j++){
+            if(nums1[j]%2==0){
+                nums2[j]=nums1[j]-odd;
+            }
+            else{
+                nums2[j]=nums1[j];
+            }
+        }
+
+        // System.out.println(Arrays.toString(nums2));
         return true;
+
     }
 }
+
+// It is always possible to make the array all odd with at least 1 odd number since even - odd = odd, if there are no odds the array is already all even so it is always possible, the answer is just return true
