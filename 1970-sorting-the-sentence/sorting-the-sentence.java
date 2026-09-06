df3361f223867bd63723b046260390e8f2456538
Shlok Warukar <warulkarshlok@gmail.com>
2026-09-06 12:55:29 +0530

    Time: 2 ms (16.75%) | Memory: 42.7 MB (80.57%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,27 +1,41 @@
+// class Solution {
+//     public String sortSentence(String s) {
+
+//         String[] words = s.split("\\s+");
+//         String result = "";
+//         String[] ans = new String[200];
+
+//         int max = 0;
+//         int min = 10;
+
+//         for (int i = words.length - 1; i >= 0; i--) {
+
+//             int digit = (words[i].charAt(words[i].length() - 1)) - '0';
+
+//             max = Math.max(max, digit);
+//             min = Math.min(min, digit);
+
+//             ans[digit - 1] = words[i].substring(0, words[i].length() - 1); //to remove the last digit
+//         }
+
+//         for (int j = min - 1; j < max; j++) {
+//             result += ans[j] + " "; //to add space between words 
+//         }
+
+//         return result.trim();
+//     }
+// } //SOLID BRUTE FORCE 3ms 
+
 class Solution {
     public String sortSentence(String s) {
-
         String[] words = s.split("\\s+");
-        String result = "";
-        String[] ans = new String[200];
+        String[] ans = new String[words.length];
 
-        int max = 0;
-        int min = 10;
-
-        for (int i = words.length - 1; i >= 0; i--) {
-
-            int digit = (words[i].charAt(words[i].length() - 1)) - '0';
-
-            max = Math.max(max, digit);
-            min = Math.min(min, digit);
-
-            ans[digit - 1] = words[i].substring(0, words[i].length() - 1); //to remove the last digit
+        for (String word : words) {
+            int digit = word.charAt(word.length() - 1) - '0';
+            ans[digit-1] = word.substring(0, word.length() - 1);
         }
 
-        for (int j = min - 1; j < max; j++) {
-            result += ans[j] + " "; //to add space between words 
-        }
-
-        return result.trim();
+        return String.join(" ", ans);
     }
 }
