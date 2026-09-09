59e9f180a5c3d2d3a709c2eec6df0be716310afb
Shlok Warukar <warulkarshlok@gmail.com>
2026-09-09 12:28:00 +0530

    Time: 1 ms (65.45%) | Memory: 45.1 MB (51.42%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public int addedInteger(int[] nums1, int[] nums2) {
+//        if(Arrays.equals(nums1,nums2)) return 0;
+
+//        Arrays.sort(nums1);
+//        Arrays.sort(nums2);
+
+//        int ans = nums2[0]-nums1[0]; 
+//        for(int i = 1;i<nums1.length;i++){
+//             if((nums2[i]-nums1[i]) != ans){
+//                 return 0;
+//             }
+//        }
+
+//         return ans;
+//     }
+// } //5ms
+
+//QUESTION NIT SMJUN GHYAYCHA N SHLOKKKKK
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-       if(Arrays.equals(nums1,nums2)) return 0;
+        int min1=nums1[0],min2=nums2[0];
 
-       Arrays.sort(nums1);
-       Arrays.sort(nums2);
+        int i=0;
+        while(i<nums1.length){
+            min1=Math.min(min1,nums1[i]);i++;
+        }
 
-       int ans = nums2[0]-nums1[0]; 
-       for(int i = 1;i<nums1.length;i++){
-            if((nums2[i]-nums1[i]) != ans){
-                return 0;
-            }
-       }
-
-        return ans;
+        i=0;
+        while(i<nums2.length){
+            min2=Math.min(min2,nums2[i]);i++;
+        }
+        return min2-min1;
     }
 }
