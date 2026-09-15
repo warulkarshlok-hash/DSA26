a9cc2794176b62044af350606b26139870ff3f3c
Shlok Warukar <warulkarshlok@gmail.com>
2026-09-15 10:27:05 +0530

    Time: 1 ms (98.46%) | Memory: 42.7 MB (55.01%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.java
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -2,8 +2,7 @@ class Solution {
     public int digitFrequencyScore(int n) {
         int sum  = 0;
         while(n>0){
-            int digit = n%10;
-            sum += digit;
+            sum += (n%10);
             n /= 10;
         }
 
